63077e0f2af72d7306070f459b3d057861fe0ffd
Nicole Hedley <nhedley@appnexus.com>
2017-07-21 16:58:16 -0400

    Xamarin calabash tests

--- PrebidMobile/IntegrationTests/features/step_definitions/calabash_steps.rb
+++ PrebidMobile/IntegrationTests/features/step_definitions/calabash_steps.rb
@@ -1,16 +1,31 @@
 require 'calabash-android/calabash_steps'
 
 Then(/^I should see AppNexus creative$/) do
-    @query_results = query("o css:'*'")
-    unless @query_results[0]['html'].include?('mbpb.js')
-      raise "Mbpb.js not found, prebid creative was not served"
+    @query_results = query("zzra css:'*'")
+    puts @query_results[0]['html']
+    unless @query_results[0]['html'].include?('pbm.js')
+      raise "Pbm.js not found, prebid creative was not served"
     end
 end
 
 Then(/^I should see AppNexus creative in PublisherAdView number (\d+)$/) do |arg1|
-  @query_results = query("o index:#{arg1} css:'*'")
-  unless @query_results[0]['html'].include?('mbpb.js')
-    raise "Mbpb.js not found, prebid creative was not served in number #{arg1} PublisherAdView"
+  @query_results = query("zzra index:#{arg1} css:'*'")
+  unless @query_results[0]['html'].include?('pbm.js')
+    raise "Pbm.js not found, prebid creative was not served in number #{arg1} PublisherAdView"
   end
+end
+
+Then(/^I should see AppNexus creative in HTMLBannerWebView$/) do
+    @query_results = query("HTMLBannerWebView css:'*'")
+    unless @query_results[0]['html'].include?('pbm.js')
+        raise "Pbm.js not found, prebid creative was not served"
+    end
+end
+
+Then(/^I should see AppNexus creative in MraidBridgeMraidWebView$/) do
+    @query_results = query("MraidActivity css:'*'")
+    unless @query_results[0]['html'].include?('pbm.js')
+        raise "Pbm.js not found, prebid creative was not served"
+    end
 end
 
