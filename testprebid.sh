ec8594f2d078c07daea9e898f653bc8052c82215
Nicole Hedley <nhedley@appnexus.com>
2017-07-24 14:18:57 -0400

    Run test-cloud in the correct directory

--- testprebid.sh
+++ testprebid.sh
@@ -6,4 +6,5 @@ cd PrebidMobile
 ./gradlew clean build
 mkdir -p IntegrationTests/apk && cp DemoApp/build/outputs/apk/DemoApp-release-unsigned.apk IntegrationTests/apk/DemoApp.apk
 gem install xamarin-test-cloud
-test-cloud submit IntegrationTests/apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
+cd IntegrationTests
+test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
