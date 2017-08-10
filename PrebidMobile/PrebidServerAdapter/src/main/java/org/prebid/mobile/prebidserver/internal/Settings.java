19275197bd03ca46f0ce6bef59c24cf639ac2708
Wei Zhang <wzhang@appnexus.com>
2017-08-10 09:48:22 -0400

    grant ability to override requset time out for http connection

--- PrebidMobile/PrebidServerAdapter/src/main/java/org/prebid/mobile/prebidserver/internal/Settings.java
+++ PrebidMobile/PrebidServerAdapter/src/main/java/org/prebid/mobile/prebidserver/internal/Settings.java
@@ -15,7 +15,7 @@ public class Settings {
     // connection settings
     public static final String REQUEST_URL_NON_SECURE = "http://prebid.adnxs.com/pbs/v1/auction";
     public static final String REQUEST_URL_SECURE = "https://prebid.adnxs.com/pbs/v1/auction";
-    public static final int REQUEST_TIME_OUR_MILLIS = 500;
+    public static int REQUEST_TIME_OUR_MILLIS = 500;
     // request keys
     public static final String REQUEST_CACHE_MARKUP = "cache_markup";
     public static final String REQUEST_SORT_BIDS = "sort_bids";
