409ff513cc5c7f4c3189c10a42d66b9f982a2120
Wei Zhang <wzhang@appnexus.com>
2017-08-15 14:20:39 -0400

    get adm from cache id

--- PrebidMobile/MediationAdaptersForMoPub/src/main/java/org/prebid/mediationadapters/mopub/FBCustomEventBanner.java
+++ PrebidMobile/MediationAdaptersForMoPub/src/main/java/org/prebid/mediationadapters/mopub/FBCustomEventBanner.java
@@ -11,11 +11,18 @@ import com.facebook.ads.AdView;
 import com.mopub.mobileads.CustomEventBanner;
 import com.mopub.mobileads.MoPubErrorCode;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.prebid.mobile.prebidserver.CacheService;
+
 import java.util.Map;
 
-public class FBCustomEventBanner extends CustomEventBanner implements AdListener {
+public class FBCustomEventBanner extends CustomEventBanner implements AdListener, CacheService.CacheListener {
     private CustomEventBannerListener customEventBannerListener;
     private AdView adView;
+    private Context context;
+    private int width;
+    private int height;
 
     @Override
     protected void loadBanner(Context context, CustomEventBannerListener customEventBannerListener, Map<String, Object> localExtras, Map<String, String> serverExtras) {
@@ -23,11 +30,10 @@ public class FBCustomEventBanner extends CustomEventBanner implements AdListener
         this.customEventBannerListener = customEventBannerListener;
         if (localExtras != null) {
             String cache_id = (String) localExtras.get("hb_cache_id");
-            int width = (int) localExtras.get("com_mopub_ad_width");
-            int height = (int) localExtras.get("com_mopub_ad_height");
-            adView = new AdView(context, "id", new AdSize(width, height));
-            adView.setAdListener(this);
-            adView.loadAdFromBid(cache_id);
+            width = (int) localExtras.get("com_mopub_ad_width");
+            height = (int) localExtras.get("com_mopub_ad_height");
+            CacheService cs = new CacheService(this, cache_id);
+            cs.execute();
         } else {
             if (customEventBannerListener != null) {
                 customEventBannerListener.onBannerFailed(MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR);
@@ -38,6 +44,7 @@ public class FBCustomEventBanner extends CustomEventBanner implements AdListener
     @Override
     protected void onInvalidate() {
         customEventBannerListener = null;
+        context = null;
         if (adView != null) {
             adView.destroy();
         }
@@ -91,5 +98,20 @@ public class FBCustomEventBanner extends CustomEventBanner implements AdListener
     @Override
     public void onLoggingImpression(Ad ad) {
 
+    }
+
+    @Override
+    public void onResponded(JSONObject jsonObject) {
+        String adm = null;
+        String placementID = "";
+        try {
+            adm = jsonObject.getString("adm");
+            JSONObject bid = new JSONObject(adm);
+            placementID = bid.getString("placement_id");
+        } catch (JSONException e) {
+        }
+        adView = new AdView(context, placementID, new AdSize(width, height));
+        adView.setAdListener(this);
+        adView.loadAdFromBid(adm);
     }
 }
