7a627de12c5c9d07e5d4d878f6d500d9d70babb2
anwzhang <wzhang@appnexus.com>
2018-01-29 17:12:06 -0500

    move to new devices for tests (#32)

--- testprebid.sh
+++ testprebid.sh
@@ -10,4 +10,5 @@ bundle install
 
 bundle exec calabash-android resign apk/DemoApp.apk
 bundle exec calabash-android build apk/DemoApp.apk
-bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
+#bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
+bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices b2a05af9 --series "master" --locale "en_US" --app-name "DemoApp" --user wzhang@appnexus.com
