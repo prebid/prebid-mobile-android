60720cfc5653c6818f0eecda8a5a30a536135a1d
Wei Zhang <wzhang@appnexus.com>
2018-04-02 14:52:16 -0400

    temporarily removing integration tests

--- testprebid.sh
+++ testprebid.sh
@@ -20,11 +20,16 @@ if [ ! -e IntegrationTests/apk/DemoApp.apk ]; then
 	echoX "file copy unsuccessful"
 fi
 
-echoX "start integration tests"
-cd IntegrationTests
-bundle install
+# Commenting Integration tests out, running it locally only for now since it requires login
+# echoX "start integration tests"
+# cd IntegrationTests
+# bundle install
+# npm install -g appcenter-cli
+# gem install xamarin-test-cloud
 
-bundle exec calabash-android resign apk/DemoApp.apk
-bundle exec calabash-android build apk/DemoApp.apk
-#bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
-bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices b2a05af9 --series "master" --locale "en_US" --app-name "DemoApp" --user wzhang@appnexus.com
+# bundle exec calabash-android resign apk/DemoApp.apk
+# bundle exec calabash-android build apk/DemoApp.apk
+# #bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices 2ae0b5a0 --series "master" --locale "en_US" --app-name "DemoApp" --user nhedley@appnexus.com
+# # bundle exec test-cloud submit apk/DemoApp.apk 435c130f3f6ff5256d19a790c21dd653 --devices b2a05af9 --series "master" --locale "en_US" --app-name "DemoApp" --user wzhang@appnexus.com
+# appcenter login
+# appcenter test run calabash --app "xtc-AppNexus/DemoApp" --devices "xtc-AppNexus/test-demo-app" --app-path apk/DemoApp.apk --test-series "master" --locale "en_US" --project-dir IntegrationTests/features
