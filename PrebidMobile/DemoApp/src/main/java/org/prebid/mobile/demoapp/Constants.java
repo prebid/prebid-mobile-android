70b2edb911092a233520b95e92be4f347403a644
Wei Zhang <wzhang@appnexus.com>
2018-05-08 14:26:32 -0400

    use a new config id so 320x50 and interstitial would work

--- PrebidMobile/DemoApp/src/main/java/org/prebid/mobile/demoapp/Constants.java
+++ PrebidMobile/DemoApp/src/main/java/org/prebid/mobile/demoapp/Constants.java
@@ -24,7 +24,7 @@ public class Constants {
     public static final String DFP_INTERSTITIAL_ADUNIT_ID_FULLSCREEN = "/19968336/PriceCheck_Interstitial";
 
     // PBS Config
-    public static final String PBS_CONFIG_APPNEXUS_DEMAND = "eebc307d-7f76-45d6-a7a7-68985169b138";
+    public static final String PBS_CONFIG_APPNEXUS_DEMAND = "625c6125-f19e-4d5b-95c5-55501526b2a4";
     public static final String PBS_CONFIG_300x250_APPNEXUS_DEMAND = "6ace8c7d-88c0-4623-8117-75bc3f0a2e45";
     public static final String PBS_ACCOUNT_ID = "bfa84af2-bd16-4d35-96ad-31c6bb888df0";
     // App Constants
