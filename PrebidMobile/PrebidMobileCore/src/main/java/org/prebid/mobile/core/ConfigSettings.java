4742f4379871569f290b733144c739b623488bb3
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2018-08-13 15:55:36 +0800

    Create ConfigSettings.java

---
+++ PrebidMobile/PrebidMobileCore/src/main/java/org/prebid/mobile/core/ConfigSettings.java
@@ -0,0 +1,63 @@
+/*
+ *    Copyright 2016 Prebid.org, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.prebid.mobile.core;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Config class sets the Config parameters like storedRequestId, priceGanularity.
+ * and other current and future features available for the auction settings.
+ */
+public class ConfigSettings {
+
+    private static PriceGranularity priceGranularity = PriceGranularity.UNKNOWN;
+    private static String storeRequestId = null;
+
+    public enum PriceGranularity {
+        LOW,
+        MED,
+        HIGH,
+        DENSE,
+        AUTO,
+        UNKNOWN
+    }
+
+    public static PriceGranularity getPriceGranularity() {
+        return priceGranularity;
+    }
+
+    public static void setPriceGranularity (PriceGranularity priceGranularity) { ConfigSettings.priceGranularity = priceGranularity; }
+
+
+    public static String getStoreRequestIdtoreRequestId() {
+        return storeRequestId;
+    }
+
+    public static void setStoreRequestId (String storeRequestId) { ConfigSettings.storeRequestId = storeRequestId; }
+
+
+
+}
