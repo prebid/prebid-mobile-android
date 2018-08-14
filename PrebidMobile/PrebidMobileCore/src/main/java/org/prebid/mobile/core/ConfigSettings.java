2cf773f7b9347a63377dbd898b5ac553b07d1eea
Antoine Jacquemin (Rubicon) <ajacquemin@rubiconproject.com>
2018-08-14 14:43:26 +0800

    Update ConfigSettings.java

--- PrebidMobile/PrebidMobileCore/src/main/java/org/prebid/mobile/core/ConfigSettings.java
+++ PrebidMobile/PrebidMobileCore/src/main/java/org/prebid/mobile/core/ConfigSettings.java
@@ -1,28 +1,46 @@
+/*
+ *    Copyright 2016 Prebid.org, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.prebid.mobile.core;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.prebid.mobile.unittestutils.BaseSetup;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
+import java.util.ArrayList;
 import java.util.Calendar;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+/**
+ * Config class sets the Config parameters like storedRequestId.
+ * and other current and future features available for the auction settings.
+ */
+public class ConfigSettings {
 
-@RunWith(RobolectricTestRunner.class)
-@Config(sdk = 21)
-public class ConfigSettingsTest extends BaseSetup {
+    private static String storeRequestId = null;
 
-    @Test
-    public void testSetStoreRequestId() {
-        assertEquals(null, ConfigSettings.getStoreRequestIdtoreRequestId());
-        ConfigSettings.setStoreRequestId("testStoredRequestId");
-        assertEquals("testStoredRequestId", ConfigSettings.getStoreRequestIdtoreRequestId());
+    public static String getStoreRequestIdtoreRequestId() {
+        return storeRequestId;
     }
+
+    public static void setStoreRequestId (String storeRequestId) { ConfigSettings.storeRequestId = storeRequestId; }
+
+
+
 }
