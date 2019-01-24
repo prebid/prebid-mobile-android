e8fb5e2a4b0c37d6818197ffbfc08d3320b671dd
Wei Zhang <wzhang@appnexus.com>
2019-01-24 17:25:30 -0500

    banner 300x250 tests for MoPub

--- PrebidMobile/API1.0Demo/src/androidTest/java/org/prebid/mobile/app/MoPubBannerTest.java
+++ PrebidMobile/API1.0Demo/src/androidTest/java/org/prebid/mobile/app/MoPubBannerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -19,35 +20,41 @@ import static androidx.test.espresso.web.matcher.DomMatchers.containingTextInBod
 import static androidx.test.espresso.web.model.Atoms.getCurrentUrl;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class DFPBannerTest {
+public class MoPubBannerTest {
     @Rule
     public ActivityTestRule<MainActivity> m = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testDFPBannerWithoutAutoRefreshAndSize300x250() throws Exception {
+    public void testMoPubWithoutAutoRefreshAndSize300x250() throws Exception {
+        onView(withId(R.id.adServerSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("MoPub"))).perform(click());
         onView(withId(R.id.autoRefreshInput)).perform(typeText("0"));
         onView(withId(R.id.showAd)).perform(click());
         Thread.sleep(5000);
         onView(withId(R.id.adFrame)).check(matches(isDisplayed()));
-        onWebView().check(webMatches(getCurrentUrl(), containsString("pubads.g.doubleclick.net/gampad/ads")));
+        onWebView().check(webMatches(getCurrentUrl(), containsString("ads.mopub.com")));
         onWebView().check(webContent(containingTextInBody("ucTag.renderAd")));
         Thread.sleep(30000);
         assertEquals(1, ((DemoActivity) TestUtil.getCurrentActivity()).refreshCount);
     }
 
     @Test
-    public void testDFPBannerWithAutoRefreshAndSize300x250() throws Exception {
+    public void testMoPubWithAutoRefreshAndSize300x250() throws Exception {
+        onView(withId(R.id.adServerSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("MoPub"))).perform(click());
         onView(withId(R.id.autoRefreshInput)).perform(typeText("30000"));
         onView(withId(R.id.showAd)).perform(click());
         Thread.sleep(5000);
         onView(withId(R.id.adFrame)).check(matches(isDisplayed()));
-        onWebView().check(webMatches(getCurrentUrl(), containsString("pubads.g.doubleclick.net/gampad/ads")));
+        onWebView().check(webMatches(getCurrentUrl(), containsString("ads.mopub.com")));
         onWebView().check(webContent(containingTextInBody("ucTag.renderAd")));
         Thread.sleep(30000);
         assertEquals(2, ((DemoActivity) TestUtil.getCurrentActivity()).refreshCount);
     }
-
 }
