c20f9270a986984f50d7979e955ea9f844101643
Wei Zhang <wzhang@appnexus.com>
2019-01-24 15:38:41 -0500

    add a default test case

--- PrebidMobile/API1.0Demo/src/androidTest/java/org/prebid/mobile/app/DFPBannerTest.java
+++ PrebidMobile/API1.0Demo/src/androidTest/java/org/prebid/mobile/app/DFPBannerTest.java
@@ -1,14 +1,10 @@
 package org.prebid.mobile.app;
 
-import android.app.Activity;
-import android.view.View;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.espresso.NoMatchingViewException;
-import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -17,28 +13,18 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.model.Atoms.getCurrentUrl;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
 import static org.hamcrest.Matchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class DFPBannerTest {
     @Rule
     public ActivityTestRule<MainActivity> m = new ActivityTestRule<>(MainActivity.class);
-
-    private static Activity getCurrentActivity() {
-        final Activity[] activity = new Activity[1];
-        onView(isRoot()).check(new ViewAssertion() {
-            @Override
-            public void check(View view, NoMatchingViewException noViewFoundException) {
-                activity[0] = (Activity) view.getContext();
-            }
-        });
-        return activity[0];
-    }
 
     @Test
     public void testDFPBannerNoAutoRefresh() throws Exception {
@@ -47,5 +33,8 @@ public class DFPBannerTest {
         Thread.sleep(5000);
         onView(withId(R.id.adFrame)).check(matches(isDisplayed()));
         onWebView().check(webMatches(getCurrentUrl(), containsString("pubads.g.doubleclick.net/gampad/ads")));
+        onWebView()
+                .withElement(findElement(Locator.TAG_NAME, "body"))
+                .check(webMatches(getText(), containsString("creative.js")));
     }
 }
