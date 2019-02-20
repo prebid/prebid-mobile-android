469b97448f531f0f05d06e8184dc373a2a95e03d
Wei Zhang <wzhang@appnexus.com>
2019-02-20 17:33:34 -0500

    retry and dismiss system dialogs

--- PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/MainActivity.java
+++ PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/MainActivity.java
@@ -139,4 +139,12 @@ public class MainActivity extends AppCompatActivity {
         }
         startActivity(demoActivityIntent);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus) {
+            sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+        }
+    }
 }
