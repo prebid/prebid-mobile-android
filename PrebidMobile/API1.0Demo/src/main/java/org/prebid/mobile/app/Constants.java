1ededc66399695a51c57f513b316af82acdf662b
Meng Luan <edmonluan@gmail.com>
2019-03-05 11:26:27 -0500

    SOR-1914

--- PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/Constants.java
+++ PrebidMobile/API1.0Demo/src/main/java/org/prebid/mobile/app/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
     static final String AUTO_REFRESH_NAME = "autoRefresh";
 
     // Prebid server config ids
-    static final String PBS_ACCOUNT_ID = "bfa84af2-bd16-4d35-96ad-31c6bb888df0";
+    static final String PBS_ACCOUNT_ID = "prebid.org";
     static final String PBS_CONFIG_ID_300x250_APPNEXUS_DEMAND = "6ace8c7d-88c0-4623-8117-75bc3f0a2e45";
     static final String PBS_CONFIG_ID_320x50_APPNEXUS_DEMAND = "625c6125-f19e-4d5b-95c5-55501526b2a4";
     static final String PBS_CONFIG_ID_INTERSTITIAL_APPNEXUS_DEMAND = "625c6125-f19e-4d5b-95c5-55501526b2a4";
