36b8ac03b6bbee80862ac75f1ac59c5ba1e599af
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2019-03-18 12:53:13 +0200

    - MIN_AUTO_REFRESH_PERIOD_MILLIS constant was created - @IntRange annotation was added

--- PrebidMobile/API1.0/src/main/java/org/prebid/mobile/AdUnit.java
+++ PrebidMobile/API1.0/src/main/java/org/prebid/mobile/AdUnit.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
@@ -27,6 +28,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public abstract class AdUnit {
+    private static final int MIN_AUTO_REFRESH_PERIOD_MILLIS = 30_000;
+
     private String configId;
     private AdType adType;
     private ArrayList<String> keywords;
@@ -40,8 +43,9 @@ public abstract class AdUnit {
         this.keywords = new ArrayList<>();
     }
 
-    public void setAutoRefreshPeriodMillis(int periodMillis) {
-        if (periodMillis < 30000) {
+    public void setAutoRefreshPeriodMillis(@IntRange(from = MIN_AUTO_REFRESH_PERIOD_MILLIS) int periodMillis) {
+        if (periodMillis < MIN_AUTO_REFRESH_PERIOD_MILLIS) {
+            LogUtil.w("periodMillis less then:" + MIN_AUTO_REFRESH_PERIOD_MILLIS);
             return;
         }
         this.periodMillis = periodMillis;
