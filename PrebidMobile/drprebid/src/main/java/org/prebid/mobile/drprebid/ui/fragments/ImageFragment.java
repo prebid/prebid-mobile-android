fb02b55fefdfe485d2e23310c9cdbfb1184a2cb2
eros902002@googlemail.com <eros902002@googlemail.com>
2019-05-07 17:43:59 +0200

    Create Welcome Screen and migrate to AndroidX

---
+++ PrebidMobile/drprebid/src/main/java/org/prebid/mobile/drprebid/ui/fragments/ImageFragment.java
@@ -0,0 +1,42 @@
+package org.prebid.mobile.drprebid.ui.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import org.prebid.mobile.drprebid.R;
+
+public class ImageFragment extends Fragment {
+    private static final String IMAGE_RESOURCE_ARG = "image_res";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (getArguments() != null && getArguments().containsKey(IMAGE_RESOURCE_ARG)) {
+            ImageView imageView = view.findViewById(R.id.view_image);
+            imageView.setImageResource(getArguments().getInt(IMAGE_RESOURCE_ARG, -1));
+        }
+    }
+
+    public static ImageFragment newInstance(int imageResource) {
+
+        Bundle args = new Bundle();
+        args.putInt(IMAGE_RESOURCE_ARG, imageResource);
+
+        ImageFragment fragment = new ImageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
