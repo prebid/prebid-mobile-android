890c9133298bef365a00c42c5a4b939e424197ba
eros902002@googlemail.com <eros902002@googlemail.com>
2019-05-27 16:38:06 +0200

    Add info screens to Test result buttons

--- PrebidMobile/drprebid/src/main/java/org/prebid/mobile/drprebid/ui/viewholders/PrebidServerValidationViewHolder.java
+++ PrebidMobile/drprebid/src/main/java/org/prebid/mobile/drprebid/ui/viewholders/PrebidServerValidationViewHolder.java
@@ -1,16 +1,29 @@
 package org.prebid.mobile.drprebid.ui.viewholders;
 
+import android.content.Intent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.prebid.mobile.drprebid.R;
+import org.prebid.mobile.drprebid.model.HelpScreen;
+import org.prebid.mobile.drprebid.ui.activities.InfoActivity;
+import org.prebid.mobile.drprebid.util.HelpScreenUtil;
 
 public class PrebidServerValidationViewHolder extends RecyclerView.ViewHolder implements TestResultViewHolder, LifecycleOwner {
 
     public PrebidServerValidationViewHolder(@NonNull final View itemView) {
         super(itemView);
+
+        itemView.findViewById(R.id.button_info).setOnClickListener(v -> {
+            HelpScreen aboutScreen = HelpScreenUtil.getRealTimeDemandTestInfo(itemView.getContext());
+            Intent intent = InfoActivity.newIntent(itemView.getContext(), aboutScreen.getTitle(), aboutScreen.getHtmlAsset());
+            itemView.getContext().startActivity(intent);
+        });
     }
 
     @Override
@@ -21,6 +34,6 @@ public class PrebidServerValidationViewHolder extends RecyclerView.ViewHolder im
     @NonNull
     @Override
     public Lifecycle getLifecycle() {
-        return null;
+        return ((AppCompatActivity) itemView.getContext()).getLifecycle();
     }
 }
