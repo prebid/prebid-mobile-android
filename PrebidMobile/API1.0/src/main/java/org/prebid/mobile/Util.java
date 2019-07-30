00ccaa4d3f689623ada8393fe56fb5edeeb4b76b
eros902002@googlemail.com <eros902002@googlemail.com>
2019-07-30 12:38:00 +0200

    Update Gradle 3.4.2

--- PrebidMobile/API1.0/src/main/java/org/prebid/mobile/Util.java
+++ PrebidMobile/API1.0/src/main/java/org/prebid/mobile/Util.java
@@ -19,14 +19,15 @@ package org.prebid.mobile;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.Size;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.Size;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
