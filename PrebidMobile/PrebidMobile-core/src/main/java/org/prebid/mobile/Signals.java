b693ae3f2249c5b2aefee0090030dc226ba39a76
Oleksandr Pavliuchyk <opavliuchyk@rubiconproject.com>
2020-04-22 14:11:08 +0300

    Signals for OpenRTB v2.5

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/Signals.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/Signals.java
@@ -42,7 +42,7 @@ public class Signals {
 
     }
     /**
-     * OpenRTB
+     * OpenRTB - API Frameworks
      * <pre>
     | Value | Description |
     |-------|-------------|
@@ -56,11 +56,22 @@ public class Signals {
      */
     public static class Api extends SingleContainerInt {
 
+        /** VPAID 1.0 */
         public static Api VPAID_1 = new Api(1);
+
+        /** VPAID 2.0 */
         public static Api VPAID_2 = new Api(2);
+
+        /** MRAID-1 */
         public static Api MRAID_1 = new Api(3);
+
+        /** ORMMA */
         public static Api ORMMA = new Api(4);
+
+        /** MRAID-2 */
         public static Api MARAID_2 = new Api(5);
+
+        /** MRAID-3 */
         public static Api MARAID_3 = new Api(6);
 
         public Api(int value) {
@@ -69,22 +80,37 @@ public class Signals {
     }
 
     /**
-     OpenRTB
+     OpenRTB - Playback Methods
      * <pre>
-     | Value | Description         |
-     |-------|---------------------|
-     | 1     | Auto-Play Sound On  |
-     | 2     | Auto-Play Sound Off |
-     | 3     | Click-to-Play       |
-     | 4     | Mouse-Over          |
+     | Value | Description                                              |
+     |-------|----------------------------------------------------------|
+     | 1     | Initiates on Page Load with Sound On                     |
+     | 2     | Initiates on Page Load with Sound Off by Default         |
+     | 3     | Initiates on Click with Sound On                         |
+     | 4     | Initiates on Mouse-Over with Sound On                    |
+     | 5     | Initiates on Entering Viewport with Sound On             |
+     | 6     | Initiates on Entering Viewport with Sound Off by Default |
      *</pre>
      */
     public static class PlaybackMethod extends SingleContainerInt {
 
+        /** Initiates on Page Load with Sound On */
         public static PlaybackMethod AutoPlaySoundOn = new PlaybackMethod(1);
+
+        /** Initiates on Page Load with Sound Off by Default  */
         public static PlaybackMethod AutoPlaySoundOff = new PlaybackMethod(2);
+
+        /** Initiates on Click with Sound On */
         public static PlaybackMethod ClickToPlay = new PlaybackMethod(3);
+
+        /** Initiates on Mouse-Over with Sound On */
         public static PlaybackMethod MouseOver = new PlaybackMethod(4);
+
+        /** Initiates on Entering Viewport with Sound On */
+        public static PlaybackMethod EnterSoundOn = new PlaybackMethod(5);
+
+        /** Initiates on Entering Viewport with Sound Off by Default */
+        public static PlaybackMethod EnterSoundOff = new PlaybackMethod(6);
 
         public PlaybackMethod(int value) {
             super(value);
@@ -92,26 +118,53 @@ public class Signals {
     }
 
     /**
-     OpenRTB
+     OpenRTB - Protocols
      * <pre>
-     | Value | Description      |
-     |-------|------------------|
-     | 1     | VAST 1.0         |
-     | 2     | VAST 2.0         |
-     | 3     | VAST 3.0         |
-     | 4     | VAST 1.0 Wrapper |
-     | 5     | VAST 2.0 Wrapper |
-     | 6     | VAST 3.0 Wrapper |
+     | Value | Description       |
+     |-------|-------------------|
+     | 1     | VAST 1.0          |
+     | 2     | VAST 2.0          |
+     | 3     | VAST 3.0          |
+     | 4     | VAST 1.0 Wrapper  |
+     | 5     | VAST 2.0 Wrapper  |
+     | 6     | VAST 3.0 Wrapper  |
+     | 7     | VAST 4.0          |
+     | 8     | VAST 4.0 Wrapper  |
+     | 9     | DAAST 1.0         |
+     | 10    | DAAST 1.0 Wrapper |
      *</pre>
      */
     public static class Protocols extends SingleContainerInt {
 
+        /** VAST 1.0 */
         public static Protocols VAST_1_0 = new Protocols(1);
+
+        /** VAST 2.0 */
         public static Protocols VAST_2_0 = new Protocols(2);
+
+        /** VAST 3.0 */
         public static Protocols VAST_3_0 = new Protocols(3);
+
+        /** VAST 1.0 Wrapper */
         public static Protocols VAST_1_0_Wrapper = new Protocols(4);
+
+        /** VAST 2.0 Wrapper */
         public static Protocols VAST_2_0_Wrapper = new Protocols(5);
+
+        /** VAST 3.0 Wrapper */
         public static Protocols VAST_3_0_Wrapper = new Protocols(6);
+
+        /** VAST 4.0 */
+        public static Protocols VAST_4_0 = new Protocols(7);
+
+        /** VAST 4.0 Wrapper */
+        public static Protocols VAST_4_0_Wrapper = new Protocols(8);
+
+        /** DAAST 1.0 */
+        public static Protocols DAAST_1_0 = new Protocols(9);
+
+        /** DAAST 1.0 Wrapper */
+        public static Protocols DAAST_1_0_WRAPPER = new Protocols(10);
 
         public Protocols(int value) {
             super(value);
@@ -119,7 +172,7 @@ public class Signals {
     }
 
     /**
-     OpenRTB
+     OpenRTB - Start Delay
      * <pre>
      | Value | Description                                      |
      |-------|--------------------------------------------------|
@@ -131,8 +184,13 @@ public class Signals {
      */
     public static class StartDelay extends SingleContainerInt {
 
+        /** Pre-Roll  */
         public static StartDelay PreRoll = new StartDelay(0);
+
+        /** Generic Mid-Roll  */
         public static StartDelay GenericMidRoll = new StartDelay(-1);
+
+        /** Generic Post-Roll  */
         public static StartDelay GenericPostRoll = new StartDelay(-2);
 
         public StartDelay(int value) {
