afaa266a88de5d5b37a204deab09cc304c0ce2e4
anwzhang <8050111+anwzhang@users.noreply.github.com>
2020-11-10 10:20:21 -0500

    Instream video (#223)
    * configure demo app with Xandr and Rubicon example
    
    * generate ad tag url for ima sdk

---
+++ Example/PrebidDemoJava/src/main/java/org/prebid/mobile/app/RubiconInstreamVideoIMADemoActivity.java
@@ -0,0 +1,164 @@
+package org.prebid.mobile.app;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ext.ima.ImaAdsLoader;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.ads.AdsMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+import org.prebid.mobile.AdUnit;
+import org.prebid.mobile.Host;
+import org.prebid.mobile.OnCompleteListener2;
+import org.prebid.mobile.PrebidMobile;
+import org.prebid.mobile.ResultCode;
+import org.prebid.mobile.Signals;
+import org.prebid.mobile.Util;
+import org.prebid.mobile.VideoAdUnit;
+import org.prebid.mobile.VideoBaseAdUnit;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class RubiconInstreamVideoIMADemoActivity extends AppCompatActivity {
+    AdUnit adUnit;
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private ImaAdsLoader adsLoader;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (adUnit != null) {
+            adUnit.stopAutoRefresh();
+            adUnit = null;
+        }
+        adsLoader.release();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instream_video);
+        playerView = findViewById(R.id.player_view);
+
+        PrebidMobile.setPrebidServerHost(Host.RUBICON);
+        PrebidMobile.setPrebidServerAccountId(Constants.PBS_ACCOUNT_ID_RUBICON);
+        PrebidMobile.setStoredAuctionResponse(Constants.PBS_STORED_RESPONSE_VAST_RUBICON);
+        VideoBaseAdUnit.Parameters parameters = new VideoBaseAdUnit.Parameters();
+        parameters.setMimes(Arrays.asList("video/mp4"));
+
+        parameters.setProtocols(Arrays.asList(Signals.Protocols.VAST_2_0));
+        // parameters.setProtocols(Arrays.asList(new Signals.Protocols(2)));
+
+        parameters.setPlaybackMethod(Arrays.asList(Signals.PlaybackMethod.AutoPlaySoundOff));
+        // parameters.setPlaybackMethod(Arrays.asList(new Signals.PlaybackMethod(2)));
+
+        parameters.setPlacement(Signals.Placement.InStream);
+        // parameters.setPlacement(new Signals.Placement(2));
+
+        VideoAdUnit adUnit = new VideoAdUnit("1001-1", 640, 480);
+        adUnit.setParameters(parameters);
+        this.adUnit = adUnit;
+        // Create an AdsLoader with the ad tag url.
+        adUnit.fetchDemand(new OnCompleteListener2() {
+            @Override
+            public void onComplete(ResultCode resultCode, Map<String, String> unmodifiableMap) {
+                String uri = Util.generateInstreamUriForGam("/19968336/Wei_instream_video", 640, 480, unmodifiableMap);
+                adsLoader = new ImaAdsLoader(RubiconInstreamVideoIMADemoActivity.this, Uri.parse(uri));
+                initializePlayer();
+            }
+        });
+    }
+
+    private void releasePlayer() {
+        if (adsLoader != null) {
+            adsLoader.setPlayer(null);
+        }
+        if (playerView != null) {
+            playerView.setPlayer(null);
+        }
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+
+    }
+
+    private void initializePlayer() {
+        // Create a SimpleExoPlayer and set is as the player for content and ads.
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+        adsLoader.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, getString(R.string.app_name));
+
+        ProgressiveMediaSource.Factory mediaSourceFactory =
+                new ProgressiveMediaSource.Factory(dataSourceFactory);
+
+        // Create the MediaSource for the content you wish to play.
+        MediaSource mediaSource =
+                mediaSourceFactory.createMediaSource(Uri.parse(getString(R.string.content_url)));
+
+        // Create the AdsMediaSource using the AdsLoader and the MediaSource.
+        AdsMediaSource adsMediaSource =
+                new AdsMediaSource(mediaSource, dataSourceFactory, adsLoader, playerView);
+
+        // Prepare the content and ad to be played with the SimpleExoPlayer.
+        player.prepare(adsMediaSource);
+
+        // Set PlayWhenReady. If true, content and ads autoplay.
+        player.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (android.os.Build.VERSION.SDK_INT <= 23 || player == null) {
+            if (playerView != null) {
+                playerView.onResume();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (android.os.Build.VERSION.SDK_INT <= 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            if (playerView != null) {
+                playerView.onPause();
+            }
+            releasePlayer();
+        }
+    }
+}
--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/Util.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/Util.java
@@ -403,8 +403,7 @@ public class Util {
             handleAdManagerCustomTargeting(bids, adObj);
         } else if (adObj.getClass() == getClassFromString(AD_MANAGER_REQUEST_BUILDER_CLASS)) {
             handleAdManagerBuilderCustomTargeting(bids, adObj);
-        }
-        else if (adObj.getClass() == HashMap.class) {
+        } else if (adObj.getClass() == HashMap.class) {
             if (bids != null && !bids.isEmpty()) {
                 HashMap map = ((HashMap) adObj);
                 map.clear();
@@ -533,7 +532,7 @@ public class Util {
 
         for (int i = 0; i < jsonArray.length(); i++) {
 
-            list.add((T)jsonArray.get(i));
+            list.add((T) jsonArray.get(i));
         }
 
         return list;
@@ -558,6 +557,25 @@ public class Util {
         }
 
         return result;
+    }
+
+    public static String generateInstreamUriForGam(String adUnit, int w, int h, Map<String, String> keywords) {
+        String uri = "https://pubads.g.doubleclick.net/gampad/ads?";
+        if (TextUtils.isEmpty(adUnit)) {
+            throw new IllegalArgumentException("adUnit should not be empty");
+        }
+        if (w <= 0 || h <= 0) {
+            throw new IllegalArgumentException("w or h should be positive numbers");
+        }
+        uri = uri + "sz=" + w + "x" + h + "&iu=" + adUnit + "&impl=s&gdfp_req=1&env=vp&output=xml_vast4&unviewed_position_start=1";
+
+        if (keywords != null) {
+            uri = uri + "&cust_params=";
+            for (String key : keywords.keySet()) {
+                uri = uri+ key + "%3D" + keywords.get(key) + "%26";
+            }
+        }
+        return uri;
     }
 
 }
