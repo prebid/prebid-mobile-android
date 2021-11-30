816de643348730e22efe01b93298e3fabb990670
Valentin Petrovych <valentin.petrovich@postindustria.com>
2021-11-30 16:22:13 +0200

    feat(new build system): refactor build script, add automatic generation of pom files

--- scripts/buildPrebidMobile.sh
+++ scripts/buildPrebidMobile.sh
@@ -1,197 +1,221 @@
 #! /bin/bash
-# This script helps to build the Prebid products in the following steps:
-# It will ask you the version you're releasing
-# Check if it's the same as the one in the project's build.gradle
-# Package releases
-# End
+### Constants
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
 
-######################
-# Helper Methods
-######################
-
-# Merge Script
+# Paths
 if [ -d "scripts" ]; then
-cd scripts/
+  cd scripts/
 fi
 
 set -e
-
 cd ..
-echo -e "$PWD"
+BASEDIR="$PWD"
+PATH_ARTIFACTS=$BASEDIR/build/generated-artifacts/org/prebid
+PATH_GENERATED=$BASEDIR/generated
+PATH_LOGS=$PATH_GENERATED/logs
+PATH_FAT=$PATH_GENERATED/fat
+PATH_TEMP=$PATH_GENERATED/temp
+PATH_AAR=build/outputs/aar
+PATH_LIBS=build/libs
 
-# Setup some constants for use later on.
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+echo "$BASEDIR"
 
+### Methods
 function echoX {
-echo -e "PREBID BUILDLOG: $@"
+  echo -e "PREBID: $@"
 }
 
-die() { echoX "$@" 1>&2 ; echoX "End Script"; exit 1;  }
+# $1 - gradle command
+function runGradle() {
+  echo
+  echoX "Running gradle command: ${1}"
+  "$BASEDIR"/gradlew "${1}"
+  # > "$PATH_LOGS"/"${1}".log 2>&1
+  # || die "Build failed, check log in $PATH_LOGS/build.log"
+  echo
+}
 
-######################
-# Build Settings
-######################
+# $1 - module name
+function generateArtifacts() {
+  echoX "Generating artifacts and javadoc for module: ${1}"
 
-# file paths
-BASEDIR="$PWD"
-OUTDIR=$BASEDIR/generated
-LOGPATH=$OUTDIR/logs
-FAT_PATH=$OUTDIR/fat
-AARPATH=build/outputs/aar
-BUILD_LIBS_PATH=build/libs
-TEMPDIR=$OUTDIR/temp
-LIBDIR=$BASEDIR
-PREBIDCORE=PrebidMobile
+  runGradle :"${1}":assemble
+  runGradle :"${1}":javadocJar
+  runGradle :"${1}":sourcesJar
+  runGradle :"${1}":publishAllPublicationsToLocalArtifactsRepository
+#  runGradle :"${1}":generateMetadataFileForLibraryPublication
+}
 
-echoX "$BASEDIR"
+# $1 - regex, $2 - file to search, $3 createDir
+function findVariableWithRegex() {
+  local foundMatch=""
 
-# set the default release version to what's in the project's build.gradle file
-RELEASE_VERSION=""
-regex="prebidVersionName.*=.*\"(.*)\""
-while read -r line;
-do
-if [[ $line =~ $regex ]];
-then
-RELEASE_VERSION=${BASH_REMATCH[1]};
-fi
-done < $LIBDIR/build.gradle
+  while read -r line; do
+    if [[ $line =~ ${1} ]]; then
+      foundMatch=${BASH_REMATCH[1]}
+    fi
+  done <"${2}"
 
-echoX "Start building Prebid Mobile version $RELEASE_VERSION"
+  echo "$foundMatch"
+}
 
-###########################
-# Prepare
-###########################
-echoX "Clean directories"
-rm -rf $OUTDIR
-mkdir $OUTDIR
-mkdir $LOGPATH
-rm -rf $TEMPDIR
-cd $LIBDIR
-./gradlew -i --no-daemon clean >$LOGPATH/clean.log 2>&1
+die() { runGradle "--stop"; echoX "$@" 1>&2; echoX "End Script"; exit 1;  }
 
-###########################
-# Generate modules
-###########################
 
+### Code
+# Set Prebid and OM SDK version from root build.gradle file
+VERSION_PREBID="$(findVariableWithRegex "prebidVersionName.*=.*\"(.*)\"" "$BASEDIR"/build.gradle)"
+VERSION_OM_SDK="$(findVariableWithRegex "omSdkVersion.*=.*\"(.*)\"" "$BASEDIR"/build.gradle)"
+echoX "Building Prebid: [${VERSION_PREBID}], OM SDK version: [${VERSION_OM_SDK}]"
+
+
+echoX "Cleaning old builds"
+rm -rf "$PATH_GENERATED"
+rm -rf "$BASEDIR/build"
+mkdir "$PATH_GENERATED"
+cd "$BASEDIR"
+runGradle "clean"
+
+
+echoX "Generating artifacts"
 modules=(
   "PrebidMobile"
   "PrebidMobile-core"
   "PrebidMobile-rendering"
+)
+artifactIds=(
+  "prebid-mobile-sdk"
+  "prebid-mobile-sdk-core"
+  "prebid-mobile-sdk-rendering"
 )
 projectPaths=(
   "$BASEDIR/PrebidMobile"
   "$BASEDIR/PrebidMobile/PrebidMobile-core"
   "$BASEDIR/PrebidMobile/PrebidMobile-rendering"
 )
+for n in "${!modules[@]}"; do
+	echoX "Work with module: ${modules[$n]}, artifact id: ${artifactIds[$n]}"
+	generateArtifacts "${modules[$n]}"
 
-for n in ${!modules[@]}; do
-
-	echo -e "\n"
-	echoX "Assembling ${modules[$n]}"
-	cd $LIBDIR
-	# clean existing build results, exclude test task, and assemble new release build
-	(./gradlew -i --no-daemon ${modules[$n]}:assembleRelease > $LOGPATH/build.log 2>&1 || die "Build failed, check log in $LOGPATH/build.log" )
-
-  # Make folder generated/temp/output
-	echoX "Packaging ${modules[$n]}"
-	mkdir $TEMPDIR
-	cd $TEMPDIR
-	mkdir output
-	
-	AARPATH_ABSOLUTE="${projectPaths[$n]}/$AARPATH"
-
-	cd $AARPATH_ABSOLUTE
-	unzip -q -o ${modules[$n]}-release.aar
-	cd $TEMPDIR/output
-
-	# Extracting the Contents of a JAR File
-	jar xf $AARPATH_ABSOLUTE/classes.jar
-	rm $AARPATH_ABSOLUTE/classes.jar
-
-	# Handle ProGuard rules from .aar into .jar
-	# rename proguard.txt to proguard.pro
-	mv $AARPATH_ABSOLUTE/proguard.{txt,pro}
-	mkdir -p $AARPATH_ABSOLUTE/META-INF
-	mkdir $AARPATH_ABSOLUTE/META-INF/proguard
-	mv $AARPATH_ABSOLUTE/proguard.pro $AARPATH_ABSOLUTE/META-INF/proguard
-	# move META-INF into a result direcotory
-	mv $AARPATH_ABSOLUTE/META-INF $TEMPDIR/output
-
-	# Creating a JAR File
-	jar cf ${modules[$n]}.jar org* META-INF*
-
-	# move jar into a result direcotory
-	mv ${modules[$n]}.jar $OUTDIR
-
-	cd $LIBDIR
-
-	# Javadoc
-	echoX "Preparing ${modules[$n]} Javadoc"
-	./gradlew -i --no-daemon ${modules[$n]}:javadocJar>$LOGPATH/javadoc.log 2>&1 || die "Build Javadoc failed, check log in $LOGPATH/javadoc.log"
-
-	# Sources
-	echoX "Preparing ${modules[$n]} Sources"
-	./gradlew -i --no-daemon ${modules[$n]}:sourcesJar>$LOGPATH/sources.log 2>&1 || die "Build Sources failed, check log in $LOGPATH/sources.log"
-
-	# copy sources and javadoc into a result direcotory
-	BUILD_LIBS_PATH_ABSOLUTE="${projectPaths[$n]}/$BUILD_LIBS_PATH"
-	cp -a $BUILD_LIBS_PATH_ABSOLUTE/. $OUTDIR/
-
-	# clean tmp dir
-	rm -r $TEMPDIR
+  # Moving aar
+	mv "${PATH_ARTIFACTS}/${artifactIds[$n]}/${VERSION_PREBID}/${artifactIds[$n]}-${VERSION_PREBID}.aar" "${PATH_GENERATED}/${artifactIds[$n]}.aar"
+	# Moving pom
+	mv "${PATH_ARTIFACTS}/${artifactIds[$n]}/${VERSION_PREBID}/${artifactIds[$n]}-${VERSION_PREBID}.pom" "${PATH_GENERATED}/${artifactIds[$n]}.pom"
+	# Moving javadoc
+	mv "${projectPaths[$n]}/build/libs/${modules[$n]}-javadoc.jar" "${PATH_GENERATED}/${artifactIds[$n]}-javadoc.jar"
+	# Moving sources$
+	mv "${projectPaths[$n]}/build/libs/${modules[$n]}-sources.jar" "${PATH_GENERATED}/${artifactIds[$n]}-sources.jar"
 done
 
-### omsdk
-echo -e "\n"
-echoX "Assembling omsdk"
 
-mkdir $TEMPDIR
-cd $TEMPDIR
-mkdir output
-cd output
-cp -a "$BASEDIR/PrebidMobile/omsdk-android/omsdk-android-1.3.17.aar" "$TEMPDIR/output"
-unzip -q -o omsdk-android-1.3.17.aar
-# Delete all files instead classes.jar
-find . ! -name 'classes.jar' -type f -exec rm -f {} +
-unzip -q -o classes.jar
-rm classes.jar
+echoX "Work with Open measurement SDK"
+mv "${PATH_ARTIFACTS}/prebid-mobile-sdk-open-measurement/${VERSION_OM_SDK}/prebid-mobile-sdk-open-measurement-${VERSION_OM_SDK}.aar" "${PATH_GENERATED}/prebid-mobile-sdk-open-measurement.aar"
+mv "${PATH_ARTIFACTS}/prebid-mobile-sdk-open-measurement/${VERSION_OM_SDK}/prebid-mobile-sdk-open-measurement-${VERSION_OM_SDK}.pom" "${PATH_GENERATED}/prebid-mobile-sdk-open-measurement.pom"
 
-jar cf omsdk.jar com*
-mv omsdk.jar $OUTDIR
-cd $LIBDIR
-rm -r $TEMPDIR
 
-# Prepare fat PrebidDemo library which can be used for LocalJar
-echo -e "\n"
-echoX "Preparing fat PrebidDemo library"
-cd $OUTDIR
-mkdir $TEMPDIR
+echoX "Completed!"
+runGradle clean
+runGradle --stop
 
-cd $TEMPDIR; 
+#  # Make generated/temp/output folder
+#	mkdir "$PATH_TEMP"
+#	cd "$PATH_TEMP"
+#	mkdir output
+#
+#	MODULE_AAR="${projectPaths[$n]}/$PATH_AAR"
+#	cd "$MODULE_AAR"
+#	unzip -q -o "${modules[$n]}"-release.aar
+#	cd "$PATH_TEMP"/output
+#
+#	# Extracting the Contents of a JAR File
+#	jar xf $MODULE_AAR/classes.jar
+#	rm $MODULE_AAR/classes.jar
+#
+#	# Handle ProGuard rules from .aar into .jar
+#	# rename proguard.txt to proguard.pro
+#	mv $MODULE_AAR/proguard.{txt,pro}
+#	mkdir -p $MODULE_AAR/META-INF
+#	mkdir $MODULE_AAR/META-INF/proguard
+#	mv $MODULE_AAR/proguard.pro $MODULE_AAR/META-INF/proguard
+#	# move META-INF into a result direcotory
+#	mv $MODULE_AAR/META-INF $PATH_TEMP/output
+#
+#	# Creating a JAR File
+#	jar cf ${modules[$n]}.jar org* META-INF*
+#
+#	# move jar into a result direcotory
+#	mv ${modules[$n]}.jar $PATH_GENERATED
+#
+#	cd $BASEDIR
+#
+#	# Javadoc
+#	echoX "Preparing ${modules[$n]} Javadoc"
+#	./gradlew -i --no-daemon ${modules[$n]}:javadocJar>$PATH_LOGS/javadoc.log 2>&1 || die "Build Javadoc failed, check log in $PATH_LOGS/javadoc.log"
+#
+#	# Sources
+#	echoX "Preparing ${modules[$n]} Sources"
+#	./gradlew -i --no-daemon ${modules[$n]}:sourcesJar>$PATH_LOGS/sources.log 2>&1 || die "Build Sources failed, check log in $PATH_LOGS/sources.log"
+#
+#	# copy sources and javadoc into a result direcotory
+#	PATH_LIBS_ABSOLUTE="${projectPaths[$n]}/$PATH_LIBS"
+#	cp -a $PATH_LIBS_ABSOLUTE/. $PATH_GENERATED/
+#
+#	# clean tmp dir
+#	rm -r $PATH_TEMP
+#done
 
-unzip -uo $OUTDIR/omsdk.jar
-unzip -uo $OUTDIR/PrebidMobile.jar
-unzip -uo $OUTDIR/PrebidMobile-core.jar
-unzip -uo $OUTDIR/PrebidMobile-rendering.jar
-
-# unzip second proguard
-unzip -B $OUTDIR/PrebidMobile.jar "META-INF/proguard/proguard.pro"
-# append text from second proguard
-cat "$TEMPDIR/META-INF/proguard/proguard.pro~" >> "$TEMPDIR/META-INF/proguard/proguard.pro"
-rm "$TEMPDIR/META-INF/proguard/proguard.pro~"
-
-rm $TEMPDIR/org/prebid/mobile/core/BuildConfig.class
-jar -cvf PrebidMobile.jar -C $TEMPDIR .
-
-mkdir $FAT_PATH
-mv PrebidMobile.jar $FAT_PATH
-
-rm -r $TEMPDIR
-
-#######
-# End
-#######
-echoX "Please find Prebid Mobile product in $OUTDIR"
-echo -e "\n${GREEN}Done!${NC} \n"
+#### omsdk
+#echo -e "\n"
+#echoX "Assembling omsdk"
+#
+#mkdir $PATH_TEMP
+#cd $PATH_TEMP
+#mkdir output
+#cd output
+#cp -a "$BASEDIR/PrebidMobile/omsdk-android/omsdk-android-1.3.17.aar" "$PATH_TEMP/output"
+#unzip -q -o omsdk-android-1.3.17.aar
+## Delete all files instead classes.jar
+#find . ! -name 'classes.jar' -type f -exec rm -f {} +
+#unzip -q -o classes.jar
+#rm classes.jar
+#
+#jar cf omsdk.jar com*
+#mv omsdk.jar $PATH_GENERATED
+#cd $BASEDIR
+#rm -r $PATH_TEMP
+#
+## Prepare fat PrebidDemo library which can be used for LocalJar
+#echo -e "\n"
+#echoX "Preparing fat PrebidDemo library"
+#cd $PATH_GENERATED
+#mkdir $PATH_TEMP
+#
+#cd $PATH_TEMP;
+#
+#unzip -uo $PATH_GENERATED/omsdk.jar
+#unzip -uo $PATH_GENERATED/PrebidMobile.jar
+#unzip -uo $PATH_GENERATED/PrebidMobile-core.jar
+#unzip -uo $PATH_GENERATED/PrebidMobile-rendering.jar
+#
+## unzip second proguard
+#unzip -B $PATH_GENERATED/PrebidMobile.jar "META-INF/proguard/proguard.pro"
+## append text from second proguard
+#cat "$PATH_TEMP/META-INF/proguard/proguard.pro~" >> "$PATH_TEMP/META-INF/proguard/proguard.pro"
+#rm "$PATH_TEMP/META-INF/proguard/proguard.pro~"
+#
+#rm $PATH_TEMP/org/prebid/mobile/core/BuildConfig.class
+#jar -cvf PrebidMobile.jar -C $PATH_TEMP .
+#
+#mkdir $PATH_FAT
+#mv PrebidMobile.jar $PATH_FAT
+#
+#rm -r $PATH_TEMP
+#
+########
+## End
+########
+#echoX "Please find Prebid Mobile product in $PATH_GENERATED"
+#echo -e "\n${GREEN}Done!${NO_COLOR} \n"
