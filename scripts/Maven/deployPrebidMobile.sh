457a5b6a9effda7ed788d0b28610f8730673e85d
Valentin Petrovych <72038591+ValentinPostindustria@users.noreply.github.com>
2021-12-03 16:19:42 +0200

    Added deploying of rendering part and updated release version (#306)
    * feat(deploy rendering): update release version to 1.13-beta1, add pom files and complete deploy script for rendering #303
    
    * feat(deploy rendering): make deploying of open measurement sdk without sources #303
    
    * feat(release version): update to 1.13.0-beta1.

--- scripts/Maven/deployPrebidMobile.sh
+++ scripts/Maven/deployPrebidMobile.sh
@@ -28,6 +28,19 @@ function mavenDeploy() {
   echoX "Please complete the release process by promoting the jar at https://oss.sonatype.org/#stagingRepositories"
 }
 
+# $1 - absolute pom path, $2 - absolute aar path
+function mavenDeployWithoutSources() {
+  echoX "Deploying ${2} on Maven..."
+
+    mvn gpg:sign-and-deploy-file "-DpomFile=${1}" "-Dfile=${2}" "-DrepositoryId=ossrh" "-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/" "-DstagingRepositoryId=ossrh" || {
+      echoX "Deploy failed!"
+      echoX "End Script"
+      exit 1
+    }
+
+  echoX "Please complete the release process by promoting the jar at https://oss.sonatype.org/#stagingRepositories"
+}
+
 BASE_DIR="$PWD"
 DEPLOY_DIR_NAME="filesToDeploy"
 DEPLOY_DIR_ABSOLUTE="$BASE_DIR/$DEPLOY_DIR_NAME"
@@ -37,16 +50,11 @@ mkdir $DEPLOY_DIR_ABSOLUTE
 
 cd ..
 sh ./buildPrebidMobile.sh
-sh ./buildPrebidMobileRendering.sh
 
 cp -r ../generated/* $DEPLOY_DIR_ABSOLUTE || true
 
-modules=("PrebidMobile" "PrebidMobile-core")
-
+modules=("PrebidMobile" "PrebidMobile-core" "PrebidMobile-rendering")
 for n in ${!modules[@]}; do
-
-  rm -r $BASE_DIR/${modules[$n]}-pom.xml.asc || true
-
   #######
   # Start
   #######
@@ -58,7 +66,6 @@ for n in ${!modules[@]}; do
   # Deploy
   #######
   module="${modules[$n]}"
-
   mavenDeploy $"$BASE_DIR/${module}-pom.xml" $"$DEPLOY_DIR_ABSOLUTE/${module}.jar" $"$DEPLOY_DIR_ABSOLUTE/${module}-sources.jar" $"$DEPLOY_DIR_ABSOLUTE/${module}-javadoc.jar"
 
   #######
@@ -69,24 +76,8 @@ for n in ${!modules[@]}; do
 done
 
 #######
-# Deploy rendering artifacts
+# Open measurement SDK
 #######
-renderingArtifacts=("prebid-mobile-sdk-rendering" "prebid-mobile-sdk-mopubAdapters" "prebid-mobile-sdk-gamEventHandlers" "omsdk-android")
-
-for n in "${!renderingArtifacts[@]}"; do
-  artifact=${renderingArtifacts[$n]}
-  artifactPath="${DEPLOY_DIR_ABSOLUTE}/rendering/${artifact}/${artifact}"
-
-  # no javadoc and sources for omsdk-android artifacts
-  if [ "${artifact}" == "omsdk-android" ]; then
-    mavenDeploy $"${artifactPath}.pom" $"${artifactPath}.aar"
-  else
-    mavenDeploy $"${artifactPath}.pom" $"${artifactPath}.aar" $"${artifactPath}-sources.jar" $"${artifactPath}-javadoc.jar"
-  fi
-done
-
-#######
-# Deploy rendering artifacts end
-#######
+mavenDeployWithoutSources $"PrebidMobile-open-measurement-pom.xml" $"$DEPLOY_DIR_ABSOLUTE/omsdk.jar"
 
 echoX "End Script"
