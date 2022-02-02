3e013bf52717ff19bb3649bb6ddf514175cab5bd
Valentin Petrovych <valentin.petrovich@postindustria.com>
2022-02-02 14:20:34 +0200

    feat(AdMob): indicate the status of interstitial ads #344

--- Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialFragment.kt
+++ Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialFragment.kt
@@ -3,7 +3,9 @@ package org.prebid.mobile.renderingtestapp.plugplay.bidding.admob
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
@@ -45,7 +47,7 @@ open class AdMobInterstitialFragment : AdFragment() {
         return adUnit
     }
 
-    open override fun loadAd() {
+    override fun loadAd() {
         val request = AdRequest
             .Builder()
             .addCustomEventExtrasBundle(PrebidInterstitialAdapter::class.java, extras!!)
@@ -61,6 +63,7 @@ open class AdMobInterstitialFragment : AdFragment() {
                     btnLoad?.text = getString(R.string.text_show)
 
                     interstitialAd = ad
+                    interstitialAd?.fullScreenContentCallback = createFullScreenContentCallback()
                 }
 
                 override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -86,7 +89,12 @@ open class AdMobInterstitialFragment : AdFragment() {
 
     private fun resetAdEvents() {
         btnAdLoaded?.isEnabled = false
+        btnAdShowed?.isEnabled = false
+        btnAdImpression?.isEnabled = false
+        btnAdDismissed?.isEnabled = false
+        btnAdFailedFullScreen?.isEnabled = false
         btnAdFailed?.isEnabled = false
+        btnAdClicked?.isEnabled = false
     }
 
     private fun handleLoadButtonClick() {
@@ -99,6 +107,30 @@ open class AdMobInterstitialFragment : AdFragment() {
             btnLoad?.isEnabled = false
             btnLoad?.text = "Loading..."
             loadAd()
+        }
+    }
+
+    private fun createFullScreenContentCallback(): FullScreenContentCallback {
+        return object : FullScreenContentCallback() {
+            override fun onAdClicked() {
+                btnAdClicked?.isEnabled = true
+            }
+
+            override fun onAdImpression() {
+                btnAdImpression?.isEnabled = true
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                btnAdShowed?.isEnabled = true
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                btnAdDismissed?.isEnabled = true
+            }
+
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                btnAdFailedFullScreen?.isEnabled = true
+            }
         }
     }
 
