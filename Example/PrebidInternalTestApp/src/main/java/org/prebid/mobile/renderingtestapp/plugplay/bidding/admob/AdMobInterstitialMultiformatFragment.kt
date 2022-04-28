4ec4b2f516fcdddb3860a7c564d07591889733e3
Valentin Petrovych <valentin.petrovich@postindustria.com>
2022-04-28 19:11:32 +0300

    fix: resolve dependency

--- Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialMultiformatFragment.kt
+++ Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialMultiformatFragment.kt
@@ -1,7 +1,7 @@
 package org.prebid.mobile.renderingtestapp.plugplay.bidding.admob
 
 import android.os.Bundle
-import org.prebid.mobile.admob.AdMobInterstitialMediationUtils
+import org.prebid.mobile.admob.AdMobMediationInterstitialUtils
 import org.prebid.mobile.rendering.bidding.display.MediationInterstitialAdUnit
 import org.prebid.mobile.rendering.bidding.enums.AdUnitFormat
 import java.util.*
@@ -10,7 +10,7 @@ open class AdMobInterstitialMultiformatFragment : AdMobInterstitialFragment() {
 
     override fun initAd(): Any? {
         extras = Bundle()
-        val mediationUtils = AdMobInterstitialMediationUtils(extras)
+        val mediationUtils = AdMobMediationInterstitialUtils(extras)
         val adUnitFormats = EnumSet.of(AdUnitFormat.DISPLAY, AdUnitFormat.VIDEO)
         adUnit = MediationInterstitialAdUnit(activity, configId, adUnitFormats, mediationUtils)
         adUnit?.setMinSizePercentage(30, 30)
