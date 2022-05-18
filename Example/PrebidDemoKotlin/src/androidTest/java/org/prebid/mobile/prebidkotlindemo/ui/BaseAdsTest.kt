72b627b061a336218823a9aedf0cd5ad1180904f
MaksPetrukhnoPI <94057303+MaksPetrukhnoPI@users.noreply.github.com>
2022-05-18 19:45:30 +0300

    configured admob display interstitial case

--- Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/BaseAdsTest.kt
+++ Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/BaseAdsTest.kt
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith
 @SdkSuppress(minSdkVersion = 18)
 abstract class BaseAdsTest {
     protected val packageName = "org.prebid.mobile.prebidkotlindemo"
-    protected val timeout = 6000L
+    protected val timeout = 5000L
     protected lateinit var device: UiDevice
 
     private lateinit var adServerSpinner: UiObject
@@ -73,7 +73,7 @@ abstract class BaseAdsTest {
 
     protected fun testAd(adServer: String, adName: String, retryCount: Int = 2) {
         goToAd(adServer, adName)
-        println("$adServer - $adName")
+
         try {
             checkAd(adServer)
             teardownAd(adServer)
