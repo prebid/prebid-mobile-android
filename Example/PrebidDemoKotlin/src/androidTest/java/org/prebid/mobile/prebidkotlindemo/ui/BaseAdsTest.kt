8433aec4667e980357e9f26d120cd2fda294575e
MaksPetrukhnoPI <94057303+MaksPetrukhnoPI@users.noreply.github.com>
2022-05-23 18:49:03 +0300

    increased timeout

--- Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/BaseAdsTest.kt
+++ Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/BaseAdsTest.kt
@@ -24,7 +24,7 @@ import androidx.test.uiautomator.UiObject
 @SdkSuppress(minSdkVersion = 18)
 abstract class BaseAdsTest {
     protected val packageName = "org.prebid.mobile.prebidkotlindemo"
-    protected val timeout = 7000L
+    protected val timeout = 8000L
     protected lateinit var device: UiDevice
 
     private lateinit var adServerSpinner: UiObject
