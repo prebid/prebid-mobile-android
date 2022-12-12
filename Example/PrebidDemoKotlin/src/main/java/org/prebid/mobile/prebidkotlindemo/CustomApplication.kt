51917af0685fa562bccf80255dbbf32f83bdf105
Valentin Petrovych <valentin.petrovich@postindustria.com>
2022-12-12 13:45:40 +0200

    feat: add sdk initialization listeners into the test apps

--- Example/PrebidDemoKotlin/src/main/java/org/prebid/mobile/prebidkotlindemo/CustomApplication.kt
+++ Example/PrebidDemoKotlin/src/main/java/org/prebid/mobile/prebidkotlindemo/CustomApplication.kt
@@ -24,9 +24,15 @@ import com.google.android.gms.ads.RequestConfiguration
 import org.prebid.mobile.Host
 import org.prebid.mobile.PrebidMobile
 import org.prebid.mobile.TargetingParams
+import org.prebid.mobile.api.exceptions.InitError
 import org.prebid.mobile.prebidkotlindemo.utils.Settings
+import org.prebid.mobile.rendering.listeners.SdkInitializationListener
 
 class CustomApplication : Application() {
+
+    companion object {
+        private const val TAG = "PrebidCustomApplication"
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -40,7 +46,15 @@ class CustomApplication : Application() {
     private fun initPrebidSDK() {
         PrebidMobile.setPrebidServerAccountId("0689a263-318d-448b-a3d4-b02e8a709d9d")
         PrebidMobile.setPrebidServerHost(Host.createCustomHost("https://prebid-server-test-j.prebid.org/openrtb2/auction"))
-        PrebidMobile.initializeSdk(applicationContext, null)
+        PrebidMobile.initializeSdk(applicationContext, object : SdkInitializationListener {
+            override fun onSdkInit() {
+                Log.d(TAG, "SDK initialized successfully!")
+            }
+
+            override fun onSdkFailedToInit(error: InitError?) {
+                Log.e(TAG, "SDK initialization error: " + error?.error)
+            }
+        })
         PrebidMobile.setShareGeoLocation(true)
     }
 
