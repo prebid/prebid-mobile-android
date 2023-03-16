cf24c57b3cc4de590bf69d98203f1bb5f0965938
Valentin Petrovych <valentin.petrovich@postindustria.com>
2023-03-16 13:49:46 +0200

    fix: multiformat examples

--- Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialMultiformatFragment.kt
+++ Example/PrebidInternalTestApp/src/main/java/org/prebid/mobile/renderingtestapp/plugplay/bidding/admob/AdMobInterstitialMultiformatFragment.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import org.prebid.mobile.admob.AdMobMediationInterstitialUtils
 import org.prebid.mobile.api.data.AdUnitFormat
 import org.prebid.mobile.api.mediation.MediationInterstitialAdUnit
+import org.prebid.mobile.renderingtestapp.R
 import java.util.*
 
 open class AdMobInterstitialMultiformatFragment : AdMobInterstitialFragment() {
 
     override fun initAd(): Any? {
+        val context = requireContext()
+
         extras = Bundle()
         val mediationUtils = AdMobMediationInterstitialUtils(extras)
         val adUnitFormats = EnumSet.of(AdUnitFormat.DISPLAY, AdUnitFormat.VIDEO)
-        adUnit = MediationInterstitialAdUnit(activity, configId, adUnitFormats, mediationUtils)
+        adUnit = MediationInterstitialAdUnit(
+            activity,
+            listOf(
+                context.getString(R.string.imp_prebid_id_interstitial_320_480),
+                context.getString(R.string.imp_prebid_id_video_interstitial_320_480)
+            ).shuffled().first(),
+            adUnitFormats,
+            mediationUtils
+        )
         adUnit?.setMinSizePercentage(30, 30)
         return adUnit
     }
