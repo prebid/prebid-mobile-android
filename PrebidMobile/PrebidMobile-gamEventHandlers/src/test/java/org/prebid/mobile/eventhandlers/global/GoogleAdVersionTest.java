4dd86a632a0119230622cf00ec06d4017f5e3231
Valentin Petrovych <valentin.petrovich@postindustria.com>
2023-03-30 13:49:32 +0300

    feat: update GMA to 22.0.0 #605

--- PrebidMobile/PrebidMobile-gamEventHandlers/src/test/java/org/prebid/mobile/eventhandlers/global/GoogleAdVersionTest.java
+++ PrebidMobile/PrebidMobile-gamEventHandlers/src/test/java/org/prebid/mobile/eventhandlers/global/GoogleAdVersionTest.java
@@ -1,13 +1,14 @@
 package org.prebid.mobile.eventhandlers.global;
 
+import static org.junit.Assert.assertEquals;
+
 import com.google.android.gms.ads.MobileAds;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.prebid.mobile.PrebidMobile;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 29)
@@ -19,7 +20,7 @@ public class GoogleAdVersionTest {
         assertEquals(
                 "Google Ad SDK was updated to " + currentVersion + "! " +
                         "Please test Prebid SDK with the new version, resolve compilation problems, rewrite deprecated code. " +
-                        "After testing you can update PrebidMobile.TESTED_GOOGLE_SDK_VERSION and update version in publishing XML files.",
+                        "After testing you can update PrebidMobile.TESTED_GOOGLE_SDK_VERSION and update version in publishing XML files (scripts/Maven/*.xml.",
                 currentVersion,
                 PrebidMobile.TESTED_GOOGLE_SDK_VERSION
         );
