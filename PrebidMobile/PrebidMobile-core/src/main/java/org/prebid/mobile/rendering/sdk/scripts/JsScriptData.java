29a1eaa70697727414bc4776800457003bd5f493
Valentin Petrovych <valentin.petrovich@postindustria.com>
2023-06-13 14:54:08 +0200

    feat: update url #634

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/scripts/JsScriptData.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/scripts/JsScriptData.java
@@ -2,14 +2,13 @@ package org.prebid.mobile.rendering.sdk.scripts;
 
 public class JsScriptData {
 
-    // TODO: Change urls
     public static final JsScriptData openMeasurementData = new JsScriptData(
             "PBMJSLibraries/omsdk.js",
-            "https://cdn.jsdelivr.net/gh/prebid/prebid-mobile-ios@master/PrebidMobile/PrebidMobileRendering/Assets/omsdk.js"
+            "https://cdn.jsdelivr.net/gh/prebid/prebid-mobile-android@master/scripts/js/omsdk_v1.js"
     );
     public static final JsScriptData mraidData = new JsScriptData(
             "PBMJSLibraries/mraid.js",
-            "https://cdn.jsdelivr.net/gh/prebid/prebid-mobile-ios@master/PrebidMobile/PrebidMobileRendering/Assets/mraid.js"
+            "https://cdn.jsdelivr.net/gh/prebid/prebid-mobile-android@master/scripts/js/mraid.js"
     );
 
     private String path;
