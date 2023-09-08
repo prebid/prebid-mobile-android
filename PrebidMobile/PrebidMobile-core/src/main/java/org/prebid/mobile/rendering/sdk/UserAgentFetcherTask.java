3c5a38de10f13f0534a769b59dffea9aedf0ccd3
Valentin Petrovych <valentin.petrovich@postindustria.com>
2023-09-08 18:57:00 +0200

    refactor: move logic #676

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/UserAgentFetcherTask.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/UserAgentFetcherTask.java
@@ -1,6 +1,5 @@
 package org.prebid.mobile.rendering.sdk;
 
-import android.os.Build;
 import android.text.TextUtils;
 import android.webkit.WebSettings;
 
@@ -24,33 +23,10 @@ public class UserAgentFetcherTask implements Runnable {
         }
 
         if (TextUtils.isEmpty(userAgent) || userAgent.contains("UNAVAILABLE")) {
-            userAgent = "Mozilla/5.0 (Linux; U; Android " + android.os.Build.VERSION.RELEASE + ";" + " " + getDeviceName() + ")";
+            userAgent = "Mozilla/5.0 (Linux; U; Android " + android.os.Build.VERSION.RELEASE + ";" + " " + AppInfoManager.getDeviceName() + ")";
         }
 
         AppInfoManager.setUserAgent(userAgent);
-    }
-
-
-    public static String getDeviceName() {
-        String manufacturer = Build.MANUFACTURER;
-        String model = Build.MODEL;
-        if (model.startsWith(manufacturer)) {
-            return capitalize(model);
-        } else {
-            return capitalize(manufacturer) + " " + model;
-        }
-    }
-
-    private static String capitalize(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-        char first = s.charAt(0);
-        if (Character.isUpperCase(first)) {
-            return s;
-        } else {
-            return Character.toUpperCase(first) + s.substring(1);
-        }
     }
 
 }
