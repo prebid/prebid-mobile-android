177e497477286deac2e2382efd302446fdc86a0a
Jono Sligh <jsligh@prebid.org>
2024-06-11 08:50:16 -0500

    listener was set to null before execution.

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/InitializationNotifier.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/sdk/InitializationNotifier.java
@@ -42,24 +42,25 @@ public class InitializationNotifier {
                     postOnMainThread(() -> {
                         listener.onInitializationComplete(InitializationStatus.SUCCEEDED);
                         listener.onSdkInit();
+                        listener = null;
                     });
                 }
             } else {
                 LogUtil.error(TAG, statusRequesterError);
 
                 if (listener != null) {
-                    InitializationStatus serverStatusWarning = InitializationStatus.SERVER_STATUS_WARNING;
-                    serverStatusWarning.setDescription(statusRequesterError);
                     postOnMainThread(() -> {
+                        InitializationStatus serverStatusWarning = InitializationStatus.SERVER_STATUS_WARNING;
+                        serverStatusWarning.setDescription(statusRequesterError);
                         listener.onInitializationComplete(serverStatusWarning);
                         listener.onSdkFailedToInit(new InitError(statusRequesterError));
+                        listener = null;
                     });
                 }
             }
 
             tasksCompletedSuccessfully = true;
             initializationInProgress = false;
-            listener = null;
         });
     }
 
