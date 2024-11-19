b6261b90366277b455143352fcb50f9a0f4421ca
Valentin Petrovych <valentin.petrovich@postindustria.com>
2024-11-19 13:11:52 +0100

    feat: OpenRTB merger

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/models/openrtb/BidRequest.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/models/openrtb/BidRequest.java
@@ -24,6 +24,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.prebid.mobile.LogUtil;
+import org.prebid.mobile.OpenRtbMerger;
 import org.prebid.mobile.PrebidMobile;
 import org.prebid.mobile.rendering.models.openrtb.bidRequests.*;
 import org.prebid.mobile.rendering.models.openrtb.bidRequests.source.Source;
@@ -41,16 +42,11 @@ public class BidRequest extends BaseBid {
     private User user = null;
     private Source source = null;
     @Nullable
-    private JSONObject arbitraryJSONConfig;
-    @Nullable
     private String ortbConfig;
     private Ext ext = null;
 
     public JSONObject getJsonObject() throws JSONException {
         JSONObject jsonObject = new JSONObject();
-        if (arbitraryJSONConfig != null) {
-            deepMerge(arbitraryJSONConfig, jsonObject);
-        }
         if (imps != null && imps.size() > 0) {
 
             JSONArray jsonArray = new JSONArray();
@@ -70,72 +66,9 @@ public class BidRequest extends BaseBid {
         toJSON(jsonObject, "source", source != null ? source.getJsonObject() : null);
         toJSON(jsonObject, "ext", ext != null ? ext.getJsonObject() : null);
         toJSON(jsonObject, "test", PrebidMobile.getPbsDebug() ? 1 : null);
-        jsonObject = mergeOrtbConfig(jsonObject);
+        jsonObject = OpenRtbMerger.globalMerge(jsonObject, ortbConfig);
         return jsonObject;
     }
-
-    private JSONObject mergeOrtbConfig(JSONObject bidRequestJson) {
-        try {
-            if (ortbConfig == null) {
-                return bidRequestJson;
-            }
-            JSONObject ortbConfigObject = new JSONObject(ortbConfig);
-            //remove protected fields
-            if (ortbConfigObject.has("regs")) {
-                ortbConfigObject.remove("regs");
-            }
-            if (ortbConfigObject.has("device")) {
-                ortbConfigObject.remove("device");
-            }
-            if (ortbConfigObject.has("geo")) {
-                ortbConfigObject.remove("geo");
-            }
-            if (ortbConfigObject.has("ext")) {
-                if (ortbConfigObject.get("ext") instanceof JSONObject) {
-                    ortbConfigObject.getJSONObject("ext").remove("gdpr");
-                    ortbConfigObject.getJSONObject("ext").remove("us_privacy");
-                    ortbConfigObject.getJSONObject("ext").remove("consent");
-                }
-            }
-            return deepMerge(ortbConfigObject, bidRequestJson);
-        } catch(Exception e) {
-            LogUtil.error("ORTBConfig is not valid JSON");
-            return bidRequestJson;
-        }
-    }
-
-    /**
-     * Merge "source" into "target". If fields have equal name, merge them recursively.
-     * @return the merged object (target).
-     */
-    private static JSONObject deepMerge(JSONObject source, JSONObject target) throws JSONException {
-        for (Iterator<String> it = source.keys(); it.hasNext(); ) {
-            String key = it.next();
-            Object value = source.get(key);
-            if (!target.has(key)) {
-                // new value for "key":
-                target.put(key, value);
-            } else {
-                // existing value for "key" - recursively deep merge:
-                if (value instanceof JSONObject) {
-                    JSONObject valueJson = (JSONObject) value;
-                    deepMerge(valueJson, target.getJSONObject(key));
-                } else if (value instanceof JSONArray) {
-                    if (target.get(key) instanceof JSONArray) {
-                        JSONArray sourceArray = (JSONArray) value;
-                        for (int i = 0; i < sourceArray.length(); i++) {
-                            target.getJSONArray(key).put(sourceArray.getJSONObject(i));
-                        }
-                    }
-                } else {
-                    target.put(key, value);
-                }
-            }
-        }
-        return target;
-    }
-
-    // Accessors to prevent NPE while maintaining null if object is not set
 
     // App
     public App getApp() {
@@ -224,15 +157,6 @@ public class BidRequest extends BaseBid {
             ext = new Ext();
         }
         return ext;
-    }
-
-    @Nullable
-    public JSONObject getArbitraryConfig() {
-        return arbitraryJSONConfig;
-    }
-
-    public void setArbitraryConfig(@Nullable JSONObject config) {
-        this.arbitraryJSONConfig = config;
     }
 
     @Nullable
