8df19b650086b91c8ed3c838f96f70018acbbe84
Valentin Petrovych <valentin.petrovich@postindustria.com>
2024-11-21 18:03:21 +0100

    feat: array merging and json comparator for tests

--- PrebidMobile/PrebidMobile-core/src/test/java/org/prebid/mobile/OpenRtbMergerTest.java
+++ PrebidMobile/PrebidMobile-core/src/test/java/org/prebid/mobile/OpenRtbMergerTest.java
@@ -1,11 +1,18 @@
 package org.prebid.mobile;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.diff.JsonDiff;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
+import org.prebid.mobile.test.utils.ResourceUtils;
 
 public class OpenRtbMergerTest {
 
@@ -25,25 +32,49 @@ public class OpenRtbMergerTest {
     }
 
     @Test
-    public void mergeIgnoreValues() throws JSONException {
+    public void mergeSensitiveData_emptyRequest() throws JSONException {
         String request = "{}";
-        String openRtb = """
-                    {
-                        "geo": {
-                            "lat": 1.0,
-                            "lon": 2.0
-                        }
-                    }
-                """;
+        String openRtb = fromResources("OpenRtbMerger/sensitive_data_fake.json");
 
         JSONObject mergedJson = merge(request, openRtb);
 
-        assertEquals(0, mergedJson.length());
+        assertJsonEquals(fromResources("OpenRtbMerger/sensitive_data_empty.json"), mergedJson.toString());
+    }
+
+    @Test
+    public void mergeSensitiveData_fullRequest() throws JSONException {
+        String request = fromResources("OpenRtbMerger/sensitive_data_real.json");
+        String openRtb = fromResources("OpenRtbMerger/sensitive_data_fake.json");
+
+        JSONObject mergedJson = merge(request, openRtb);
+
+        assertJsonEquals(request, mergedJson.toString());
     }
 
     private JSONObject merge(String request, String openRtb) throws JSONException {
         JSONObject requestJson = new JSONObject(request);
         return OpenRtbMerger.globalMerge(requestJson, openRtb);
+    }
+
+    private String fromResources(String path) {
+        return ResourceUtils.convertResourceToString(path);
+    }
+
+    private void assertJsonEquals(String json1, String json2) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode beforeNode = mapper.readTree(json1);
+            JsonNode afterNode = mapper.readTree(json2);
+
+            String difference = JsonDiff.asJson(beforeNode, afterNode).toString();
+            if (difference.equals("[]")) {
+                return;
+            }
+
+            throw new AssertionError("Json values are different: " + difference);
+        } catch (JsonProcessingException e) {
+            throw new NullPointerException("Can't compare: " + e.getMessage());
+        }
     }
 
 }
