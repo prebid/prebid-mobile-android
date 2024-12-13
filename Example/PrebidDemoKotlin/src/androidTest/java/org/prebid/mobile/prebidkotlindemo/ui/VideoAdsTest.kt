b09b87e3de46f9b3bbcaefe731e0853cda3a1c70
Valentin Petrovych <valentin.petrovich@postindustria.com>
2024-12-13 11:52:53 +0100

    Revert "fix: smoke tests"
    This reverts commit a6c24492a3ce1403fa0622b7f6d1dae692de6232.

--- Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/VideoAdsTest.kt
+++ Example/PrebidDemoKotlin/src/androidTest/java/org/prebid/mobile/prebidkotlindemo/ui/VideoAdsTest.kt
@@ -37,8 +37,6 @@ class VideoAdsTest(
             ),
             arrayOf(R.string.in_app_video_rewarded, "In-App Video Rewarded"),
         )
-
-        const val REWARDED_CLOSE_BUTTON_WAIT_TIME = 130_000L
     }
 
     @Test
@@ -71,11 +69,11 @@ class VideoAdsTest(
         val findEndCard = device.wait(Until.findObject(endCard), timeout)
         if (findEndCard == null) {
             val endCardCloseButtonPattern = By.clazz(".*ImageView".toPattern())
-            val endCardButton = device.wait(Until.findObject(endCardCloseButtonPattern), REWARDED_CLOSE_BUTTON_WAIT_TIME)
+            val endCardButton = device.wait(Until.findObject(endCardCloseButtonPattern), timeout)
             assertNotNull(endCardButton)
         }
 
-        val findCloseButton = device.wait(Until.findObject(closeButton), REWARDED_CLOSE_BUTTON_WAIT_TIME)
+        val findCloseButton = device.wait(Until.findObject(closeButton), timeout)
         assertNotNull(findCloseButton)
         findCloseButton.click()
         Thread.sleep(1000)
@@ -116,11 +114,11 @@ class VideoAdsTest(
         val findEndCard = device.wait(Until.findObject(endCard), timeout * 3)
         if (findEndCard == null && testCase.integrationKind != IntegrationKind.GAM_ORIGINAL) {
             val endCardCloseButtonPattern = By.clazz(".*ImageView".toPattern())
-            val endCardButton = device.wait(Until.findObject(endCardCloseButtonPattern), REWARDED_CLOSE_BUTTON_WAIT_TIME)
+            val endCardButton = device.wait(Until.findObject(endCardCloseButtonPattern), timeout)
             assertNotNull(endCardButton)
         }
 
-        val findCloseButton = device.wait(Until.findObject(closeButton), REWARDED_CLOSE_BUTTON_WAIT_TIME)
+        val findCloseButton = device.wait(Until.findObject(closeButton), timeout)
         assertNotNull(findCloseButton)
         findCloseButton.click()
         Thread.sleep(1000)
