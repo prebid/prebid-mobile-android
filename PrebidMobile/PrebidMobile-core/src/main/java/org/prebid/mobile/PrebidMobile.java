42a2740d4f4db5e9c039eb97a0290862ccbd486c
Maksym Danylov <maksym.danylov@sigma.software>
2025-03-13 15:01:07 +0200

    Bumps GAM SDK version.

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/PrebidMobile.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/PrebidMobile.java
@@ -108,7 +108,7 @@ public class PrebidMobile {
     /**
      * Tested Google SDK version.
      */
-    public static final String TESTED_GOOGLE_SDK_VERSION = "24.0.0";
+    public static final String TESTED_GOOGLE_SDK_VERSION = "24.1.0";
 
     /**
      * Please use {@link PrebidMobile#setLogLevel(LogLevel)}, this field will become private in next releases.
