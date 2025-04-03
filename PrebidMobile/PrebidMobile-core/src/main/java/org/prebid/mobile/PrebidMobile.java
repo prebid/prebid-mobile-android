0c321ca882dbfc3d91b79d9274280452b1f0f955
cyu03 <chris.yu@yahooinc.com>
2025-04-02 19:03:56 -0700

    adding skip status check flag for init

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/PrebidMobile.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/PrebidMobile.java
@@ -154,6 +154,7 @@ public class PrebidMobile {
     private static int creativeFactoryTimeoutPreRenderContent = DEFAULT_PRERENDER_TIMEOUT;
     @NonNull
     private static WeakReference<PrebidEventDelegate> eventDelegateReference = new WeakReference<>(null);
+    private static boolean skipStatusCheck = false;
 
     private PrebidMobile() {
     }
@@ -614,6 +615,22 @@ public class PrebidMobile {
      */
     public static void setEventDelegate(@Nullable PrebidEventDelegate eventDelegate) {
         eventDelegateReference = new WeakReference<>(eventDelegate);
+    }
+
+    /**
+     * @param skipStatusCheck boolean flag for skipping status check
+     * If true, the SDK will not check the PBS status during initialization. This will save initialization time
+     * if the PBS endpoint is always live and handled client side
+     */
+    public static void setSkipStatusCheck(boolean skipStatusCheck) {
+        PrebidMobile.skipStatusCheck = skipStatusCheck;
+    }
+
+    /**
+     * {@link #setSkipStatusCheck(boolean)}
+     */
+    public static boolean shouldSkipStatusCheck() {
+        return skipStatusCheck;
     }
 
     /**
