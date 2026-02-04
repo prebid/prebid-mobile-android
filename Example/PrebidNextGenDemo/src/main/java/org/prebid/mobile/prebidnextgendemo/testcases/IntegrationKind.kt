66e6da8cb2eea617a2fc2302693602b9915e2d95
postindustria-code <oss@postindustria.com>
2026-02-04 15:59:05 +0200

    - Create a foundation for Next Gen SDK demo app

---
+++ Example/PrebidNextGenDemo/src/main/java/org/prebid/mobile/prebidnextgendemo/testcases/IntegrationKind.kt
@@ -0,0 +1,10 @@
+package org.prebid.mobile.prebidnextgendemo.testcases
+
+enum class IntegrationKind(
+    val adServer: String
+) {
+
+    GAM_ORIGINAL("GAM (Original API)"),
+    GAM_RENDERING("GAM (Rendering API)");
+
+}
---
+++ Example/PrebidNextGenDemo/src/main/java/org/prebid/mobile/prebidnextgendemo/testcases/TestCaseRepository.kt
@@ -0,0 +1,11 @@
+package org.prebid.mobile.prebidnextgendemo.testcases
+
+object TestCaseRepository {
+
+    lateinit var lastTestCase: TestCase
+
+    fun getList() = arrayListOf<TestCase>(
+
+    )
+
+}
